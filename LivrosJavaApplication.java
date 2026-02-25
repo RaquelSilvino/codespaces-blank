a8810caf01db502745329b4317b578462a97d9f0
Raquel.alvestec <raquel.silvinoalunounip@gmail.com>
2026-02-24 21:45:52 -0300

    Add files via upload

---
+++ LivrosJavaApplication.java
@@ -0,0 +1,12 @@
+package com.projeto.LivrosJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosJavaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosJavaApplication.class, args);
+	}
+
+}

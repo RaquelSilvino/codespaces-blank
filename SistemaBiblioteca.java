eaff668bf8b52af6dd7e661d1cec4bd34159d256
Raquel.alvestec <raquel.silvinoalunounip@gmail.com>
2026-02-24 21:46:17 -0300

    Add files via upload

---
+++ SistemaBiblioteca.java
@@ -0,0 +1,11 @@
+package model;
+
+import model.Livro;
+
+public static void main(String[] args) {
+    public static void main(String[] args) {
+        Livro livro = new Livro(1, "Java Básico", "Autor Teste", 2024, 59.90);
+        System.out.println(livro);
+    }
+     
+  
